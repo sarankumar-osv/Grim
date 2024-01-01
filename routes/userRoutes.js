11fc0e50ab4edf64e25201c34b2d10f7e129e680
iamgrim07 <155176224+iamgrim07@users.noreply.github.com>
2024-01-01 13:57:49 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -180,4 +180,91 @@ router.post('/userLogin', async (req, res) => {
   }
 });
 
+router.get("/sendMail", async (req, res) => {
+  try {
+    const users = await User.find({}, "email");
+    const userEmails = users.map((user) => user.email);
+    console.log("All user emails:", userEmails);
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "verifyuserofficial@gmail.com",
+        pass: "wsdv megz vecp wzen",
+      },
+    });
+
+    const mailOptions = {
+      from: "verifyuserofficial@gmail.com",
+      subject: "Happy Diwali 🎉.",
+    };
+
+    for (let i = 0; i < userEmails.length; i++) {
+      mailOptions.to = userEmails[i];
+      mailOptions.html = `
+      <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <title>Welcome to Our Community!</title>
+      <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f4f4f4;
+        margin: 0;
+        padding: 0;
+      }
+      .container {
+        max-width: 600px;
+        margin: 20px auto;
+        padding: 20px;
+        background-color: #ffffff;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      }
+      h1 {
+        color: #ff9900;
+        text-align: center;
+      }
+      p {
+        margin-bottom: 15px;
+      }
+      .footer {
+        margin-top: 20px;
+        font-size: 12px;
+        color: #777777;
+        text-align: center;
+      }
+    </style>
+    </head>
+    <body>
+  <div class="container">
+    <h1>HAPPY DIWALI 🎉</h1>
+    <p>Dear User,</p>
+    <p>May the divine light of Diwali spread into your life peace, prosperity, happiness, and good health. Wishing you a Happy Diwali!</p>
+    <p>If you have any questions or need further assistance, please feel free to <a href="mailto:verifyuserofficial@gmail.com" style="color: #007bff; text-decoration: none;">contact us</a>.</p>
+    <p>Best regards,<br>Saran Kumar.</p>
+    <div class="footer">
+      This is an automated message. Please do not reply to this email.
+    </div>
+  </div>
+</body>
+    </html>
+    `;
+
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          console.log(`Error sending email to ${userEmails[i]}:`, err);
+        } else {
+          console.log(`Email sent successfully to ${userEmails[i]}.`);
+        }
+      });
+    }
+
+    res.send("Mail Sent Successful...");
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.error("Error:", err.message);
+  }
+});
+
 module.exports = router;
